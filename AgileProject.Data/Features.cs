12148e6013e2b31d43a547c0a914283427617dd5
Johnson Lee <johnson.hwang1008@gmail.com>
2021-11-11 19:25:38 -0500

    johnson

---
+++ AgileProject.Data/Features.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgileProject.Data
+{
+    public class Features
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string FeatureName { get; set; }
+        [Required]
+        public bool HasWeapon { get; set; }
+        [Required]
+        public bool HasArmor { get; set; }
+    }
+}
