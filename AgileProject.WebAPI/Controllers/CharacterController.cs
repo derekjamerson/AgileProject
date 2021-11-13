4b0617c75686e043223312912e4f0d12c02fec22
Ashley Fellows <ashley.r.fellows@gmail.com>
2021-11-13 10:10:57 -0500

    started character controller

---
+++ AgileProject.WebAPI/Controllers/CharacterController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AgileProject.WebAPI.Controllers
+{
+    public class CharacterController : ApiController
+    {
+        private readonly CharacterDbContext _context = new CharacterDbContext();
+
+
+    }
+}
