9578676bda558ead277372b9e355bd1324ce23d7
Ashley Fellows <ashley.r.fellows@gmail.com>
2021-11-13 10:29:29 -0500

    added dbcontext, still need to add fam + features

---
+++ AgileProject.Models/AgileDbContext.cs
@@ -0,0 +1,20 @@
+﻿using AgileProject.Data;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgileProject.Models
+{
+    public class AgileDbContext : DbContext
+    {
+        public AgileDbContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Character> MyProperty { get; set; }
+    }
+}
